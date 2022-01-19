4553cb5d32e5e1c70102f89113262a18ea3e25f3
Xuebin Ji <bingojxb@sina.cn>
2022-01-19 23:49:57 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,12 +1,5 @@
 var member=[
-{"phone":"No.1","name":"范闲"},
-{"phone":"No.2","name":"张庆"},
-{"phone":"No.3","name":"林婉儿"},
-{"phone":"No.4","name":"海棠朵朵"},
-{"phone":"No.5","name":"范若若"},
-{"phone":"No.6","name":"司理理"},
-{"phone":"No.7","name":"五竹"},
-{"phone":"No.8","name":"云之澜"},
-{"phone":"No.9","name":"叶教授"},
-{"phone":"No.10","name":"叶轻眉"}
+{"phone":"","name":"范闲"},
+{"phone":"","name":"张庆"},
+{"phone":"","name":"林婉儿"}
 ]
